bfba205655ba279d3125fe4fe9adefee870f8b09
Joshua Yeung <najoshua.yeung@gmail.com>
2026-02-07 00:29:45 -0500

    revamp26

--- Makefile
+++ Makefile
@@ -1,17 +1,20 @@
 PREFIX ?= /usr/local
-CC ?= cc
+CC ?= gcc
+CFLAGS = -O2 -march=native -Wno-unused-result -Wno-incompatible-pointer-types
 LDFLAGS = -lX11
 
 output: dwmblocks.c blocks.def.h blocks.h
-	${CC}  dwmblocks.c $(LDFLAGS) -o dwmblocks
+	${CC} ${CFLAGS} dwmblocks.c $(LDFLAGS) -o dwmblocks
+
 blocks.h:
 	cp blocks.def.h $@
 
-
 clean:
-	rm -f *.o *.gch dwmblocks
+	rm -f *.o *.gch dwmblocks blocks.h
+
 install: output
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	install -m 0755 dwmblocks $(DESTDIR)$(PREFIX)/bin/dwmblocks
+
 uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/bin/dwmblocks
--- blocks.h
+++ blocks.h
@@ -1,13 +1,29 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// blocks.def.h
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" RAM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    /*Icon*/    /*Command*/                                        /*Interval*/  /*Signal*/
+    
+    // 1. Pacman Updates (Icon: Arch Linux Logo)
+    {" ", "checkupdates | wc -l",                                 3600,         0},
 
-	{"VOL: ", "/home/joshua/.scripts/volume.sh",					                            1,		0},
+    // 2. CPU Load (Icon: Microchip)
+    {" ", "cat /proc/loadavg | awk '{print $1}'",                 5,            0},
 
-	{"", "date '+%b %d (%a) %H:%M:%S'",				                    	      1,		0},
+    // 3. Disk Space (Icon: Hard Drive)
+    {" ", "df -h / | awk '/\\// { print $4 }'",                   60,           0},
+
+    // 4. Memory (Icon: RAM Stick)
+    {" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30,        0},
+
+    // 5. Battery (Icon: Battery)
+    {" ", "cat /sys/class/power_supply/BAT*/capacity",            30,           0},
+
+    // 6. Volume (Icon is inside the script)
+    {"", "/home/joshua/.scripts/volume.sh",                        0,            10},
+
+    // 7. Date (Icon: Clock) - Changed to 24hr format (%H:%M)
+    {" ", "date '+%b %d (%a) %H:%M'",                             5,            0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// Delimiter
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
